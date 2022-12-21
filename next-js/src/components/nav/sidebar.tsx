a6abcffc2eb2eef589313637c11fc89514b5a086
Freekieb7 <freekieb7@hotmail.com>
2022-12-21 19:37:34 +0100

    somehow start working button padding ruins html

--- next-js/src/components/nav/sidebar.tsx
+++ next-js/src/components/nav/sidebar.tsx
@@ -8,71 +8,48 @@ type Props = {
 
 export default function Sidebar({ open, toggleOpen }: Props) {
   return (
-    <Transition.Root show={open} as={Fragment}>
-      <Dialog as="div" className="relative z-10" onClose={toggleOpen}>
+    <Transition.Root show={open}>
+      <Dialog open={open} onClose={toggleOpen}>
         <Transition.Child
-          as={Fragment}
-          enter="ease-in-out duration-500"
-          enterFrom="opacity-0"
-          enterTo="opacity-100"
-          leave="ease-in-out duration-500"
-          leaveFrom="opacity-100"
-          leaveTo="opacity-0"
+          enter="transition-opacity ease-linear duration-300"
+          enter-from="opacity-0"
+          enter-to="opacity-100"
+          leave="transition-opacity ease-linear duration-300"
+          leave-from="opacity-100"
+          leave-to="opacity-0"
         >
           <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
         </Transition.Child>
 
-        <div className="overflow-hidden pointer-events-none pr-10">
-          <Transition.Child
-            as={Fragment}
-            enter="ease-in-out duration-500"
-            enterFrom="opacity-0"
-            enterTo="opacity-100"
-            leave="ease-in-out duration-500"
-            leaveFrom="opacity-100"
-            leaveTo="opacity-0"
-          >
-            <Dialog.Panel className="sidebar-height pointer-events-auto absolute w-screen max-w-md">
-              {/* <Transition.Child
-                    as={Fragment}
-                    enter="ease-in-out duration-500"
-                    enterFrom="opacity-0"
-                    enterTo="opacity-100"
-                    leave="ease-in-out duration-500"
-                    leaveFrom="opacity-100"
-                    leaveTo="opacity-0"
-                  >
-                    <div className="absolute top-0 left-0 -ml-8 flex pt-4 pr-2 sm:-ml-10 sm:pr-4">
-                      <button
-                        type="button"
-                        className="rounded-md text-gray-300 hover:text-white focus:outline-none focus:ring-2 focus:ring-white"
-                        onClick={() => setOpen(false)}
-                      >
-                        <span className="sr-only">Close panel</span>
-                        <XMarkIcon className="h-6 w-6" aria-hidden="true" />
-                      </button>
-                    </div>
-                  </Transition.Child> */}
-              <div className="flex h-full flex-col overflow-y-scroll bg-white py-6 shadow-xl">
-                <div className="px-4 sm:px-6">
-                  <Dialog.Title className="text-lg font-medium text-gray-900">
-                    Panel title
-                  </Dialog.Title>
-                </div>
-                <div className="relative mt-6 flex-1 px-4 sm:px-6">
-                  {/* Replace with your content */}
-                  <div className="absolute inset-0 px-4 sm:px-6">
-                    <div
-                      className="h-full border-2 border-dashed border-gray-200"
-                      aria-hidden="true"
-                    />
-                  </div>
-                  {/* /End replace */}
-                </div>
+        <Transition.Child
+          as="div"
+          enter="transition-opasity ease-linear duration-200 transform"
+          enter-from="-translate-x-full"
+          enter-to="translate-x-0"
+          leave="transition ease-in-out duration-200 transform"
+          leave-from="translate-x-0"
+          leave-to="-translate-x-full"
+        >
+          <Dialog.Panel className="sidebar-height pointer-events-auto absolute w-screen max-w-md">
+            <div className="flex h-full flex-col overflow-y-scroll bg-white py-6 shadow-xl">
+              <div className="px-4 sm:px-6">
+                <Dialog.Title className="text-lg font-medium text-gray-900">
+                  Panel title
+                </Dialog.Title>
               </div>
-            </Dialog.Panel>
-          </Transition.Child>
-        </div>
+              <div className="relative mt-6 flex-1 px-4 sm:px-6">
+                {/* Replace with your content */}
+                <div className="absolute inset-0 px-4 sm:px-6">
+                  <div
+                    className="h-full border-2 border-dashed border-gray-200"
+                    aria-hidden="true"
+                  />
+                </div>
+                {/* /End replace */}
+              </div>
+            </div>
+          </Dialog.Panel>
+        </Transition.Child>
       </Dialog>
     </Transition.Root>
   );
